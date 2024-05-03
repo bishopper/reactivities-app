1dab63fcfe36aacfb652d82e4064c8566fa31d07
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-03 20:49:37 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Header } from "semantic-ui-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [activities, SetActivities] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		axios.get("https://localhost:7263/api/activities").then((response) => {
+			console.log(response);
+			SetActivities(response.data);
+		});
+	}, []);
+
+	return (
+		<>
+			<Header as="h2" icon="users" content="reactivites App" />
+
+			<ul>
+				{activities.map((activity: any) => (
+					<li key={activity.id}>{activity.title}</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
