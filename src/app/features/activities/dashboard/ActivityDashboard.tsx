577801626bdc49d82afdd1c89218911ff05c1040
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-04 00:55:10 +0330

    Update ActivityDashboard.tsx

--- src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
 	activities: Activity[];
@@ -21,6 +22,7 @@ const ActivityDashboard = ({ activities }: Props) => {
 				</Grid.Column>
 				<Grid.Column width="6">
 					{activities[0] && <ActivityDetails activity={activities[0]} />}
+					<ActivityForm />
 				</Grid.Column>
 			</Grid>
 		</>
