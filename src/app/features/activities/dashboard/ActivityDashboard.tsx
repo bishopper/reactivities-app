339de78944ccf04f3e2743d44fad67785b63bf71
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-05-04 12:55:15 +0330

    Update ActivityDashboard.tsx

--- src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,23 +6,51 @@ import ActivityForm from "../form/ActivityForm";
 
 interface Props {
 	activities: Activity[];
+	selectedActivity: Activity | undefined;
+	selectActivity: (id: string) => void;
+	cancelSelectedActivity: () => void;
+	editMode: boolean;
+	openForm: (id?: string) => void;
+	closeForm: () => void;
 }
 
-const ActivityDashboard = ({ activities }: Props) => {
+const ActivityDashboard = ({
+	activities,
+	selectedActivity,
+	selectActivity,
+	cancelSelectedActivity,
+	editMode,
+	openForm,
+	closeForm,
+}: Props) => {
 	return (
 		<>
 			<Grid>
 				<Grid.Column width="10">
 					<List>
-						<ActivityList activities={activities} />
+						<ActivityList
+							activities={activities}
+							selectActivity={selectActivity}
+						/>
 						{activities.map((activity) => (
 							<List.Item key={activity.id}>{activity.title}</List.Item>
 						))}
 					</List>
 				</Grid.Column>
 				<Grid.Column width="6">
-					{activities[0] && <ActivityDetails activity={activities[0]} />}
-					<ActivityForm />
+					{selectedActivity && !editMode && (
+						<ActivityDetails
+							activity={selectedActivity}
+							cancelSelectedActivity={cancelSelectedActivity}
+							openForm={openForm}
+						/>
+					)}
+					{editMode && (
+						<ActivityForm
+							closeForm={closeForm}
+							selectedActivity={selectedActivity}
+						/>
+					)}
 				</Grid.Column>
 			</Grid>
 		</>
